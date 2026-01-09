04f2fc649b69d18b05d387ab0c45b626ea1d0344
metintrn <metinturandevlog@gmail.com>
2026-01-10 02:33:45 +0300

    Add files via upload
    Today I started coding with Python, and this is my first small project.
    It is a simple band name generator that asks the user a few questions and then creates a band name.
    I made this project to practice Python basics like input, print, and variables.
    It may be a small project, but it is an important step in my coding journey.

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city_you_grow_up=input("What's the name of the city you grew up in?\t:")
+pet_name=input("What's your pet's name?\t:")
+print(f"Your band name could be{city_you_grow_up} {pet_name}")
