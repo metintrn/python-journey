4d6dee3e17832396f72ff077b4fb3b41de2f6af7
metintrn <metinturandevlog@gmail.com>
2026-01-10 02:37:05 +0300

    Add comments to day1.py for project explanation
    Added comments explaining the purpose of the project.

--- day1.py
+++ day1.py
@@ -2,3 +2,9 @@ print("Welcome to the Band Name Generator.")
 city_you_grow_up=input("What's the name of the city you grew up in?\t:")
 pet_name=input("What's your pet's name?\t:")
 print(f"Your band name could be{city_you_grow_up} {pet_name}")
+
+
+#Today I started coding with Python, and this is my first small project.
+#It is a simple band name generator that asks the user a few questions and then creates a band name.
+#I made this project to practice Python basics like input, print, and variables.
+#It may be a small project, but it is an important step in my coding journey.
